a4090c3777623035d31ebe3a54b4da9667deb5de
frankxieke <kxie@streamnative.io>
2021-10-20 10:34:00 +0800

    fix npe

--- src/main/java/org/apache/pulsar/ecosystem/io/deltalake/DeltaLakeConnectorConfig.java
+++ src/main/java/org/apache/pulsar/ecosystem/io/deltalake/DeltaLakeConnectorConfig.java
@@ -56,6 +56,9 @@ public class DeltaLakeConnectorConfig implements Serializable {
         this.startingVersion = "";
         this.startingTimeStamp = "";
         this.tablePath = "";
+        this.fileSystemType = "";
+        this.s3aSecretKey = "";
+        this.s3aAccesskey = "";
     }
 
     /**
